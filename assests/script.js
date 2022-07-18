b4355c73c20cd155ae2feeab4429e92b9125e126
Combatxd <logan.schoerner@gmail.com>
2022-07-18 13:57:43 -0600

    WorkingBtnClickAndColorChange

--- assests/script.js
+++ assests/script.js
@@ -4,17 +4,34 @@ var saveBtn = $(".saveBtn");
 var today = moment().format("MMMM Do, YYYY");
 $("#currentDay").text(today);
 
-function timeBlockColor() {
+function changeTimeBlock() {
     var hour = moment().hours();
 
-    $(".time-block")
+    $(".time-block").each(function (){
+        var currentHour =parseInt($(this).attr("id"))
+
+        if(currentHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (currentHour === hour) {
+            $(this).addClass("present")
+        }
+        if (currentHour < hour)
+            $(this).addClass("past")
+    })
 }
 
 saveBtn.on("click", function() {
-
+    var timeOfDay = $(this).siblings(".hour").text();
+    var plan = $(this).siblings("textArea").val();
+    
+    localStorage.setItem(timeOfDay, plan);
 })
 
 
 function savedPlans() {
 
 }
+
+changeTimeBlock();
+savedPlans();
