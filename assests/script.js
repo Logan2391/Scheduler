7e58dc5bd2181124ff5d0a3030674435fc41b707
Combatxd <logan.schoerner@gmail.com>
2022-07-18 15:50:42 -0600

    ClearButton

--- assests/script.js
+++ assests/script.js
@@ -1,8 +1,21 @@
 var saveBtn = $(".saveBtn");
+var clearBtn = $(".clearBtn");
 
+// presensts the time of day at the top of the pagee 
 
 var today = moment().format("MMMM Do, YYYY");
 $("#currentDay").text(today);
+
+// saves the plan and time to local storage on click
+
+saveBtn.on("click", function() {
+    var timeOfDay = $(this).siblings(".hour").text();
+    var plan = $(this).siblings("textArea").val();
+    
+    localStorage.setItem(timeOfDay, plan);
+})
+
+// changes the color of the time block based on the time of day 
 
 function changeTimeBlock() {
     var hour = moment().hours();
@@ -21,17 +34,25 @@ function changeTimeBlock() {
     })
 }
 
-saveBtn.on("click", function() {
-    var timeOfDay = $(this).siblings(".hour").text();
-    var plan = $(this).siblings("textArea").val();
-    
-    localStorage.setItem(timeOfDay, plan);
+// ensures the plans saved in local storage persist 
+
+$("#9 textarea").val(localStorage.getItem("9AM"));
+$("#10 textarea").val(localStorage.getItem("10AM"));
+$("#11 textarea").val(localStorage.getItem("11AM"));
+$("#12 textarea").val(localStorage.getItem("12AM"));
+$("#13 textarea").val(localStorage.getItem("1PM"));
+$("#14 textarea").val(localStorage.getItem("2PM"));
+$("#15 textarea").val(localStorage.getItem("3PM"));
+$("#16 textarea").val(localStorage.getItem("4PM"));
+$("#17 textarea").val(localStorage.getItem("5PM"));
+
+// clears localStorage and resest values in the textarea so its empty
+
+clearBtn.on("click", function(){
+    localStorage.clear(); 
+    $("textarea").val('');
 })
 
-
-function savedPlans() {
-
-}
+// calls the color change function
 
 changeTimeBlock();
-savedPlans();
