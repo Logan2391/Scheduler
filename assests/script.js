1bc1f9b11ec9ecd3a352b87f67ec02eb55229c1c
Combatxd <logan.schoerner@gmail.com>
2022-07-18 13:08:02 -0600

    basicFunctionsLayedOut

--- assests/script.js
+++ assests/script.js
@@ -1,2 +1,20 @@
+var saveBtn = $(".saveBtn");
+
+
 var today = moment().format("MMMM Do, YYYY");
 $("#currentDay").text(today);
+
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block")
+}
+
+saveBtn.on("click", function() {
+
+})
+
+
+function savedPlans() {
+
+}
